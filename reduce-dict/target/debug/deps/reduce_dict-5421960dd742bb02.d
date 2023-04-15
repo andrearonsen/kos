349f4eb75a0b44384c6740665182d4ac8aa95851
André Fagerlie Aronsen <andre.aronsen@gmail.com>
2023-04-15 17:35:00 +0200

    Clean

--- reduce-dict/target/debug/deps/reduce_dict-5421960dd742bb02.d
+++
@@ -1,5 +0,0 @@
-/Users/andre/src/kos/reduce-dict/target/debug/deps/reduce_dict-5421960dd742bb02: src/main.rs
-
-/Users/andre/src/kos/reduce-dict/target/debug/deps/reduce_dict-5421960dd742bb02.d: src/main.rs
-
-src/main.rs:
--- reduce-dict/target/debug/reduce-dict.d
+++
@@ -1,1 +0,0 @@
-/Users/andre/src/kos/reduce-dict/target/debug/reduce-dict: /Users/andre/src/kos/reduce-dict/src/main.rs
